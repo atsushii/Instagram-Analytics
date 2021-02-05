3b02c6c1f2ddc0dd01f40c93bb36b5fb492a1480
Atsushi <atsushimiyamoto07@gmail.com>
2021-02-04 17:36:09 -0800

    Update admin usr support

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser')}),
+        ('important dates'), {'fileds': ('last_login',)}
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',)
+            'fields'L('email', 'password1', 'password2')
+        }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
