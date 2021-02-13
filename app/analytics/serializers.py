548212be6c82a49f220c4c00d22232192478f965
Atsushi <atsushimiyamoto07@gmail.com>
2021-02-12 18:44:18 -0800

    Created serializer

---
+++ app/analytics/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from core.models import InstagramAccount, InstagramComment
+
+
+class InstagramAccountSerializer(serializers.ModelSerializer):
+    """Serializer for instagram account"""
+
+    class Meta:
+        model = InstagramAccount
+        fields = ('id', 'tag')
+
+
+class InstagramCommentSerializer(serializers.ModelSerializer):
+    """Serializer for comments"""
+
+    class Meta:
+        model = InstagramComment
+        fields = ('id',
+                  'from_username',
+                  'comment',
+                  'created_time')
