64039f89090e900518230acbaca0dca46c448dcc
Atsushi <atsushimiyamoto07@gmail.com>
2021-03-05 13:25:56 -0800

    added models to admin page

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from core import models
+from core.models import User, \
+                        InstagramAccount, \
+                        InstagramMedia, \
+                        InstagramComment, \
+                        InstagramMediaTag, \
+                        InstagramMediaLocation
 
 
 class UserAdmin(BaseUserAdmin):
@@ -20,4 +25,54 @@ class UserAdmin(BaseUserAdmin):
     )
 
 
-admin.site.register(models.User, UserAdmin)
+class InstagramAccountAdmin(admin.ModelAdmin):
+    fields = (
+        'id', 'counts_followed_by', 'profile_picture_url',
+        'counts_media', 'counts_follow', 'user'
+    )
+    list_display = (
+        'id', 'counts_followed_by', 'profile_picture_url',
+        'counts_media', 'counts_follow', 'user'
+    )
+    readonly_fields = (
+        'id', 'counts_followed_by', 'profile_picture_url',
+        'counts_media', 'counts_follow'
+    )
+
+
+class InstagramMediaAdmin(admin.ModelAdmin):
+    fields = (
+        'id', 'created_time', 'link_to_media',
+        'comments_count', 'media_type', 'links_count',
+        'user'
+    )
+    list_display = (
+        'id', 'created_time', 'link_to_media',
+        'comments_count', 'media_type', 'links_count',
+        'user'
+    )
+    readonly_fields = (
+        'id', 'created_time', 'link_to_media',
+        'comments_count', 'media_type', 'links_count'
+    )
+
+
+class InstagramCommentAdmin(admin.ModelAdmin):
+    fields = (
+        'id', 'from_username', 'comment',
+        'created_time', 'media'
+    )
+    list_display = (
+        'id', 'from_username', 'comment',
+        'created_time', 'media'
+    )
+    readonly_fields = (
+        'id', 'from_username', 'comment',
+        'created_time', 'media'
+    )
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(InstagramAccount, InstagramAccountAdmin)
+admin.site.register(InstagramMedia, InstagramMediaAdmin)
+admin.site.register(InstagramComment, InstagramCommentAdmin)
